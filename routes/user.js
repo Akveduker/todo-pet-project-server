92232811ceb07df057dfa32aa04cf9fe8f532224
Nick akveduk <nikkap2001@mail.ru>
2023-08-21 20:25:30 +0300

    Новый рут для юзера
    Добавил новый get рут для юзера

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { Router, json } = require("express");
 const User = require('../schemas/user')
+const validateToken = require('../utils/validateToken.js')
 
 
 const router = Router()
@@ -20,5 +21,19 @@ router.patch('/user', jsonParser, async (req, res) => {
     }
 
 })
+
+
+router.get('/user', jsonParser, validateToken, async (req, res) => {
+    const { id } = req.body.authorization
+    try {
+        const user = await User.findById(id)
+        if (!user) return res.status(404).send('Пользователь не найден')
+        return res.status(200).send(user)
+    } catch (e) {
+        res.sendStatus(500);
+    }
+}
+)
+
 
 module.exports = router
