c0d036f16a1141f185d99fd2459e14d4a526134e
Nick akveduk <nikkap2001@mail.ru>
2023-08-21 20:25:30 +0300

    Новый рут для юзера и прака старого
    Добавил новый get рут для юзера и добавил валидатор заголовка в старый
    рут

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,39 @@
 const { Router, json } = require("express");
 const User = require('../schemas/user')
+const validateToken = require('../utils/validateToken.js')
 
 
 const router = Router()
 const jsonParser = json()
 
 
-router.patch('/user', jsonParser, async (req, res) => {
-    const { _id, ...restData } = req.body
+router.patch('/user', jsonParser, validateToken, async (req, res) => {
+    const { authorization, ...restData } = req.body
+    const { id: _id } = authorization
     try {
         const user = await User.findOneAndUpdate({ _id, ...restData })
         if (user) {
             const newUser = await User.findOne({ _id })
-            res.status(200).send(newUser)
+            return res.status(200).send(newUser)
         }
-        else res.status(404).send('Пользователь не найден')
+        return res.status(404).send('Пользователь не найден')
     } catch (e) {
         res.sendStatus(500);
     }
 
 })
+
+router.get('/user', jsonParser, validateToken, async (req, res) => {
+    const { id } = req.body.authorization
+    try {
+        const user = await User.findById(id)
+        if (!user) return res.status(404).send('Пользователь не найден')
+        return res.status(200).send(user)
+    } catch (e) {
+        res.sendStatus(500);
+    }
+}
+)
+
 
 module.exports = router
