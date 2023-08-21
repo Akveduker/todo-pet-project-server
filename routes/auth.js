1dc7765d598cb42a15f35d81fb463365f4e80092
Nick akveduk <nikkap2001@mail.ru>
2023-08-21 14:19:32 +0300

    Изменение /auth и /reg на работу с jwt токеном
    Теперь руты будт возвращать объект юзера и токен

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router, json } = require("express");
+const generateTokenFromId = require('../utils/generateAccessToken')
 const User = require('../schemas/user')
 
 
@@ -10,10 +11,16 @@ router.post('/auth', jsonParser, async (req, res) => {
     const { email, password } = req.body
     try {
         const user = await User.findOne({ email, password })
-        if (user) res.send(user)
-        else res.status(404).send('Пользователь не найден')
+
+        if (!user) return res.status(404).send('Пользователь не найден')
+
+        const token = generateTokenFromId(user._id)
+        return res.status(200).send({
+            token,
+            data: user
+        })
     } catch (e) {
-        res.sendStatus(500);
+        return res.sendStatus(500);
     }
 
 })
@@ -22,15 +29,18 @@ router.post('/reg', jsonParser, async (req, res) => {
     const { email, password, name, userColor } = req.body
     try {
         const user = await User.findOne({ email, password })
-        if (user) {
-            res.status(400).send('Пользователь уже зарегистрированн')
-            return
-        }
+        if (user) return res.status(400).send('Пользователь уже зарегистрированн')
         try {
             const user = await User.create({ email, password, name, userColor })
-            if (user) res.status(200).send(user)
+            if (user) {
+                const token = generateTokenFromId(user._id)
+                return res.status(200).send({
+                    token,
+                    data: user
+                })
+            }
         } catch (e) {
-            res.status(500)
+            return res.status(500)
         }
     } catch (e) {
         res.sendStatus(500);
